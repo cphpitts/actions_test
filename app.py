6001636dd991b6659666ab5dcc6fb0d7703e67f0
cphpitts <cphpitts@users.noreply.github.com>
2023-03-29 16:16:20 -0700

    Blah

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
-def print_string(str):
-    print(str)
+"""
+This is a trash module
+"""
+
+def print_string(text) -> None:
+    """
+    Prints a thing
+
+    :param text: str, text to print
+    """
+    print(text)
 
 print_string("Hello World")
